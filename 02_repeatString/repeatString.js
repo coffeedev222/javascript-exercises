a67bcd8fba422e6fc6043b58b3f7cd5851169f22
coffeedev <168582458+coffeedev222@users.noreply.github.com>
2024-06-05 11:51:24 -0500

    Updated function to take string, and number parameters, checks for negative value, then returns the string x number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringToPass, numberOfTimesToLoop) {
+    let loopString = '';
+    if(numberOfTimesToLoop < 0) {
+        return 'ERROR'
+    }
+    else{
+        for(i=0; i < numberOfTimesToLoop; i++) {
+            loopString += stringToPass ;
+        }
+    }
+    return loopString;
 };
 
 // Do not edit below this line
