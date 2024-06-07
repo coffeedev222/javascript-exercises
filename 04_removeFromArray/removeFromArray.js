526b636a7769e4008651f2f40eef8bef7950d44a
coffeedev <168582458+coffeedev222@users.noreply.github.com>
2024-06-07 16:02:39 -0500

    Created function that will loop through an array and the given arguements to remove any item that matches in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArgs, ...args) {
+    let newArray = [];
+    for(let i=0; i < givenArgs.length; i++){
+      let itemToKeep = true;
+      for(let j=0; j < args.length; j++) {
+        if(givenArgs[i] === args[j]){
+          itemToKeep = false;
+          break;
+        }
+      }
+      if(itemToKeep) {
+        newArray.push(givenArgs[i])
+      }
+    }
+  return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
