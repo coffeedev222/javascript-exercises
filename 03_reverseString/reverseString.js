b67af0aae892436041cb4fcea81f3865e3e238a4
coffeedev <168582458+coffeedev222@users.noreply.github.com>
2024-06-07 12:11:11 -0500

    wrote function to take a given string, store the given word in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(givenString) {
+    let reverseLetters = '';
+    let reverseLetterLength = givenString.length - 1;
+    for(i = 0; i < givenString.length; i++) {
+        reverseLetters += givenString[reverseLetterLength];
+        reverseLetterLength--;
+    }
+    return reverseLetters;
 };
 
 // Do not edit below this line
